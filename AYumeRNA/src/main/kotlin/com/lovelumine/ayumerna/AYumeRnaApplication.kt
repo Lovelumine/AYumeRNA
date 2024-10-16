aeedfc18f68ba4a78ae18aec018211b48abbb5b0
lovelumine <shendekoudai@lovelumine.com>
2024-10-16 21:17:53 +0800

    第一次提交

---
+++ AYumeRNA/src/main/kotlin/com/lovelumine/ayumerna/AYumeRnaApplication.kt
@@ -0,0 +1,11 @@
+package com.lovelumine.ayumerna
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class AYumeRnaApplication
+
+fun main(args: Array<String>) {
+    runApplication<AYumeRnaApplication>(*args)
+}
